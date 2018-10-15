812d8e6c9d2c98e049b6300278beb46389694ce4
Vasikaran S <vasikaran.25@gmail.com>
2018-10-15 18:53:32 +0530

    example added

--- example/src/components/Popup.js
+++ example/src/components/Popup.js
@@ -4,46 +4,47 @@ import PopupItem from './PopupItem';
 import Scroll from './Scroll';
 import style from '../css/app.css';
 
-export default class Popup extends React.Component{
-	constructor(props) {
-		super(props);
-		this.state = {
-			isViewPopup: false
-		};
-		this.togglePopup = this.togglePopup.bind(this);
-	}
+export default class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isViewPopup: false
+    };
+    this.togglePopup = this.togglePopup.bind(this);
+  }
 
-	togglePopup(){
-		let { isViewPopup } = this.state;
-		this.setState({
-			isViewPopup: !isViewPopup
-		})
-	}
+  togglePopup() {
+    let { isViewPopup } = this.state;
+    this.setState({
+      isViewPopup: !isViewPopup
+    });
+  }
 
-	render(){
-		return <div>
-			<button onClick={this.togglePopup}>Seach Agent</button>
-				{
-					this.state.isViewPopup && <Scroll className={style.popup}>{
-							[1,2,3,4,5,6,7,8,9,10,11,12,13].map(id=>{
-							return <PopupItem agent={this.props.agent} key={id}/>
-						})
-					}
-						<Scroll className={style.popup1}>{
-								[1,2,3,4,5,6,7,8,9,10,11,12,13].map(id=>{
-								return <PopupItem agent={this.props.agent} key={id}/>
-							})
-						}
-						</Scroll>
-					</Scroll>
-				}
-		</div>
-	}
+  render() {
+    return (
+      <div>
+        <button onClick={this.togglePopup}>Seach Agent</button>
+        {this.state.isViewPopup && (
+          <Scroll className={style.popup}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(id => {
+              return <PopupItem agent={this.props.agent} key={id} />;
+            })}
+            <Scroll className={style.popup1}>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(id => {
+                return <PopupItem agent={this.props.agent} key={id} />;
+              })}
+            </Scroll>
+          </Scroll>
+        )}
+      </div>
+    );
+  }
 }
 
 Popup.defaultProps = {
   agent: {
-  		name: "Vasi",
-  		pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-  	}
-}
+    name: 'Raina',
+    pic:
+      'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+  }
+};
--- example/src/defaultProps.js
+++ example/src/defaultProps.js
@@ -1,197 +1,238 @@
 var obj = {
-  "menus": ['Ticketlist', 'Agent popup'],
-  "ticketDetails": [
-		{
-			title: "For testing",
-			id: "#101",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#102",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#103",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#104",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#105",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#106",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#107",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#108",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#109",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#110",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#111",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#112",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#113",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#114",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#115",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#116",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#117",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#118",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#119",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#120",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		},{
-			title: "For testing",
-			id: "#121",
-			content: "Sample ticket",
-			"status": "Open",
-			ticketOwner: {
-				name: "Vasikaran",
-				pic: "https://s13.postimg.org/qkcr430vr/vasi.jpg.jpg"
-			}
-		}
-  	]
-}
+  menus: ['Ticketlist', 'Agent popup'],
+  ticketDetails: [
+    {
+      title: 'For testing',
+      id: '#101',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#102',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#103',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#104',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#105',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#106',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#107',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#108',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#109',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#110',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#111',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#112',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#113',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#114',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#115',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#116',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#117',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#118',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#119',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#120',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    },
+    {
+      title: 'For testing',
+      id: '#121',
+      content: 'Sample ticket',
+      status: 'Open',
+      ticketOwner: {
+        name: 'Suresh Raina',
+        pic:
+          'https://thesportsrush.com/wp-content/uploads/2018/02/sureshraina-1517120265-770x514.jpg'
+      }
+    }
+  ]
+};
 
 export default obj;
